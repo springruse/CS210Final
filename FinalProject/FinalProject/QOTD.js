0b570adbba9d667c7e840c70c5d2365500fefa01
springruse <152939330+springruse@users.noreply.github.com>
2025-11-04 09:12:40 -0700

    first commit

---
+++ FinalProject/FinalProject/QOTD.js
@@ -0,0 +1,13 @@
+
+
+const api_url ="https://zenquotes.io/api/quotes/";
+
+async function getapi(url)
+{
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+}
+
+getapi(api_url);
+
